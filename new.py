703389e255e49fc9cc6b904e72aef179f2f44b29
Vaibhav <vaibhavchougule475@gmail.com>
2025-01-21 15:46:07 +0530

    intial

--- new.py
+++ new.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string, render_template, request ,jsonify
+from flask import Flask, render_template_string, render_template, request, jsonify
 import numpy as np
 import plotly.graph_objects as go
 
@@ -30,12 +30,12 @@ base_z = [0] * len(base_x)  # Base height is 0
 
 top_x = [p[0] for p in top_points.values()]
 top_y = [p[1] for p in top_points.values()]
-top_z = [5] * len(top_x)  # Top height is 5
-
+top_z = [5] * len(top_x)  # Default top height is 5
 
 # Function to create a 3D Stewart Platform plot
-def create_stewart_platform(offset_x=0, offset_y=0, offset_z=0, rotation=0 ,act_value=[0,0,0,0,0,0]):
-    print("xxxxxxxxxx")
+def create_stewart_platform(offset_x=0, offset_y=0, offset_z=0, rotation=0, actuator_height=5):
+    # Adjust the height of the top platform
+    adjusted_top_z = [z + actuator_height - 5 for z in top_z]  # Adjust height of actuators
     
     # Apply offsets and rotation to the top platform
     rotation_matrix = np.array([
@@ -45,19 +45,10 @@ def create_stewart_platform(offset_x=0, offset_y=0, offset_z=0, rotation=0 ,act_
     ])
     top_points_rotated = np.dot(
         rotation_matrix,
-        np.vstack((top_x, top_y, np.array(top_z) + offset_z))
+        np.vstack((top_x, top_y, np.array(adjusted_top_z) + offset_z))
     )
     top_points_rotated[0] += offset_x
     top_points_rotated[1] += offset_y
-    #   actuator number,value
-    print("abc:" , act_value[0]);
-    top_points_rotated[2][0] += act_value[0];
-   
-    top_points_rotated[2][1] += act_value[1]
-    top_points_rotated[2][2] += act_value[2]
-    top_points_rotated[2][3] += act_value[3]
-    top_points_rotated[2][4] += act_value[4]
-    top_points_rotated[2][5] += act_value[5]
 
     # Create the figure
     fig = go.Figure()
@@ -86,7 +77,6 @@ def create_stewart_platform(offset_x=0, offset_y=0, offset_z=0, rotation=0 ,act_
 
     # Add actuators
     for i in range(6):
-        print("tpr:" , top_points_rotated[2]);
         fig.add_trace(go.Scatter3d(
             x=[base_x[i], top_points_rotated[0][i]],
             y=[base_y[i], top_points_rotated[1][i]],
@@ -94,17 +84,14 @@ def create_stewart_platform(offset_x=0, offset_y=0, offset_z=0, rotation=0 ,act_
             mode='lines',
             line=dict(color='red', width=4),
             name=f'Actuator {i + 1}',
-            
         ))
-        
 
     # Update layout
     fig.update_layout(
         scene=dict(
             xaxis=dict(range=[-0.3, 0.3]),
             yaxis=dict(range=[-0.3, 0.3]),
-            zaxis=dict(range=[-0.3, 6]),
-            # zaxis=dict(range=[-0.3, actuator_height + 1]),  # Adjust Z-axis range
+            zaxis=dict(range=[-0.3, actuator_height + 1]),  # Adjust Z-axis range
         ),
         title="Custom Stewart Platform Simulation",
     )
@@ -112,15 +99,51 @@ def create_stewart_platform(offset_x=0, offset_y=0, offset_z=0, rotation=0 ,act_
     return fig
 
 
-# Routes onward
-# initail route / figure
+# Initial route / figure
 @app.route('/')
 def index():
-    print(request.host);
     # Create the default plot
     fig = create_stewart_platform()
     plot_div = fig.to_html(full_html=False)
-    return render_template('index.html', plot_div=plot_div , ip=request.host)
+    return render_template_string("""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Stewart Platform</title>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    </head>
+    <body>
+        <h1>Custom Stewart Platform Simulation</h1>
+        <form id="control-form">
+            <label for="offset_x">Offset X:</label>
+            <input type="number" id="offset_x" name="offset_x" step="0.01"><br>
+            <label for="offset_y">Offset Y:</label>
+            <input type="number" id="offset_y" name="offset_y" step="0.01"><br>
+            <label for="offset_z">Offset Z:</label>
+            <input type="number" id="offset_z" name="offset_z" step="0.01"><br>
+            <label for="rotation">Rotation (radians):</label>
+            <input type="number" id="rotation" name="rotation" step="0.01"><br>
+            <label for="actuator_height">Actuator Height:</label>
+            <input type="number" id="actuator_height" name="actuator_height" step="0.1" value="5"><br>
+            <button type="button" id="update-btn">Update Plot</button>
+        </form>
+        <div id="plot-container">{{ plot_div|safe }}</div>
+
+        <script>
+            $(document).ready(function () {
+                $('#update-btn').on('click', function () {
+                    const formData = $('#control-form').serialize();
+                    $.post('/update_plot', formData, function (response) {
+                        $('#plot-container').html(response.plot_html);
+                    }).fail(function () {
+                        alert('Error updating the plot. Please check your inputs.');
+                    });
+                });
+            });
+        </script>
+    </body>
+    </html>
+    """, plot_div=plot_div)
 
 
 @app.route('/update_plot', methods=['POST'])
@@ -130,28 +153,15 @@ def update_plot():
         offset_y = float(request.form.get('offset_y', 0))
         offset_z = float(request.form.get('offset_z', 0))
         rotation = float(request.form.get('rotation', 0))
-        act_1 = float(request.form.get('act_1' , 0))
-        act_2 = float(request.form.get('act_2' , 0))
-        act_3 = float(request.form.get('act_3' , 0))
-        act_4 = float(request.form.get('act_4' , 0))
-        act_5 = float(request.form.get('act_5' , 0))
-        act_6 = float(request.form.get('act_6' , 0))
-
-        act_value = [act_1,act_2,act_3,act_4,act_5,act_6]
-
-        print("x:" , offset_x , " y:" , offset_y)
+        actuator_height = float(request.form.get('actuator_height', 5))  # New field
 
         # Create the updated plot
-        fig = create_stewart_platform(offset_x, offset_y, offset_z, rotation , act_value)
+        fig = create_stewart_platform(offset_x, offset_y, offset_z, rotation, actuator_height)
         fig_html = fig.to_html(full_html=False)
 
         return jsonify({'plot_html': fig_html})
     except ValueError:
         return jsonify({'error': 'Invalid input values'}), 400
-
-
-
-
 
 
 if __name__ == "__main__":
